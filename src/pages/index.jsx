093880878de7b924d799c307e1075f9c25e8d240
Vendula Konečná <VendaKonecna@gmail.com>
2025-04-30 12:55:21 +0200

    hotovo

---
+++ src/pages/index.jsx
@@ -0,0 +1,22 @@
+import { render } from '@czechitas/render';
+import '../global.css';
+import './index.css';
+
+import { Header } from '../components/Header';
+import { Intro } from '../components/Intro';
+import { Pose } from '../components/Pose'; 
+
+document.querySelector('#root').innerHTML = render(
+  <div className="container">
+    <Header title="Jogíni" />
+    <Intro heading="Vítejte mezi Jogíny" text="`Naše lekce jsou zaměřeny na potřeby klientů, kteří mají odvahu zkusit
+         něco nového. Cvičíme v pomalém tempu s podrobným slovním doprovodem.
+         Postupně se seznámíte se základními principy jógy, jak přístupovat k
+         sobě i ostatním. Krok za krokem objevíte a dostanete pod kontrolu
+         svoje tělo, pocity a emoce.`" />
+    <Pose imgLocation="yoga-pose.jpg" />
+    <body>
+      <div id="root"></div>
+    </body> 
+  </div>
+);
